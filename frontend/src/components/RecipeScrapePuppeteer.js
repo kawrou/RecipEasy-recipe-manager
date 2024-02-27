0489eecd0227cb3b31fe7acaa5ee258cab1df504
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-27 14:19:08 +0000

    recipe scraper puppeteer installed

---
+++ frontend/src/components/RecipeScrapePuppeteer.js
@@ -0,0 +1,30 @@
+import puppeteer from 'puppeteer';
+
+const url = "https://www.bbcgoodfood.com/recipes/vintage-chocolate-chip-cookies";
+
+const main = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const jsonLdData = await page.$$eval('script[type="application/ld+json"]', scripts => {
+        const jsonData = [];
+
+        scripts.forEach(script => {
+            try {
+                const parsedData = JSON.parse(script.textContent);
+                jsonData.push(parsedData);
+            } catch (error) {
+                console.error('Error parsing JSON-LD data:', error);
+            }
+        });
+
+        return jsonData;
+    });
+
+    console.log(jsonLdData);
+
+    await browser.close();
+};
+
+main();
