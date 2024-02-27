6949cac410d8811c418b1314f84bbc5963104aee
Reece Owen <reece_owen@hotmail.co.uk>
2024-02-27 12:44:58 +0000

    More changes and some testing.

--- frontend/tests/pages/homePage.test.jsx
+++ frontend/tests/pages/homePage.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
 import { HomePage } from "../../src/pages/Home/HomePage";
+import { expect } from "vitest";
 
 describe("Home Page", () => {
   test("welcomes you to the site", () => {
@@ -13,28 +14,36 @@ describe("Home Page", () => {
     );
 
     const heading = screen.getByRole("heading");
-    expect(heading.textContent).toEqual("Welcome to Acebook!");
+    expect(heading.textContent).toEqual("Welcome to RecipEasy!");
+    const searchbar = screen.getByPlaceholderText("Paste your URL here");
+    expect(searchbar).toBeInTheDocument();
+    const generateRecipeBtn = screen.getByText("Generate Recipe");
+    expect(generateRecipeBtn).toBeInTheDocument();
+    const enterMaunallyBtn = screen.getByText("Enter Manually");
+    expect(enterMaunallyBtn).toBeInTheDocument();
+    const paragraph = screen.getByText("A place to store all your favourite recipes, from ones you find online to creating your own.")
+    expect(paragraph).toBeInTheDocument()
   });
 
-  test("Displays a signup link", async () => {
-    render(
-      <BrowserRouter>
-        <HomePage />
-      </BrowserRouter>
-    );
+  // test("Displays a signup link", async () => {
+  //   render(
+  //     <BrowserRouter>
+  //       <HomePage />
+  //     </BrowserRouter>
+  //   );
 
-    const signupLink = screen.getByText("Sign Up");
-    expect(signupLink.getAttribute("href")).toEqual("/signup");
-  });
+  //   const signupLink = screen.getByText("Sign Up");
+  //   expect(signupLink.getAttribute("href")).toEqual("/signup");
+  // });
 
-  test("Displays a login link", async () => {
-    render(
-      <BrowserRouter>
-        <HomePage />
-      </BrowserRouter>
-    );
+  // test("Displays a login link", async () => {
+  //   render(
+  //     <BrowserRouter>
+  //       <HomePage />
+  //     </BrowserRouter>
+  //   );
 
-    const loginLink = screen.getByText("Log In");
-    expect(loginLink.getAttribute("href")).toEqual("/login");
-  });
+  //   const loginLink = screen.getByText("Log In");
+  //   expect(loginLink.getAttribute("href")).toEqual("/login");
+  // });
 });
