61e00d7d387a29b02ac20e20b74b55da69809670
ben-dh3 <benjaminduffield1997@gmail.com>
2024-02-27 12:02:52 +0000

    tailwind setup

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -5,7 +5,7 @@ import "./HomePage.css";
 export const HomePage = () => {
   return (
     <div className="home">
-      <h1>Welcome to Acebook!</h1>
+      <h1 className="text-5xl font-bold underline">Welcome to Acebook!</h1>
       <Link to="/signup">Sign Up</Link>
       <Link to="/login">Log In</Link>
     </div>
