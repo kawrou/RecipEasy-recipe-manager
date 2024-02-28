c68ed6600666c9d1d694dd7fbdef0f3e6259980f
ben-dh3 <benjaminduffield1997@gmail.com>
2024-02-28 11:00:46 +0000

    updates from main

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 // on smaller screens burger menu button is shown - toggle open/closed
