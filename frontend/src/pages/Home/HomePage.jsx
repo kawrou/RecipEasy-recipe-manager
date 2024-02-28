04f9d9d0a9c1ef9fc736f905baf84e3ed6fd4c53
Alan Gardiner <121672581+kawrou@users.noreply.github.com>
2024-02-28 23:53:20 +0000

    Integration: HomePage, RecipeScraper, (service) scrapeRecipe. Fix: Test

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -3,39 +3,54 @@ import { useState } from "react";
 import "./HomePage.css";
 import RecipeScraper from "../../components/RecipeScraper";
 
-
-
 export const HomePage = () => {
   //Will need this for setting token later
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const navigate = useNavigate(); 
-  const[url, setUrl] = useState ("")
+  const navigate = useNavigate();
 
+  // WHY URL STATE IS HANDLED BY PARENT COMPONENT
+  // Current implementation has the parent component handle the navigation
+  // This means the url <input> of RecipeScraper component has to be handled
+  // by parent component instead.
+  // The actual FETCH request is handled by the RecipeScraper component.
 
-  const handleSubmit = async (e)=> {
-    e.preventDefault()
+  // handleSubmit is passed as prop to RecipeScraper so that when the form is
+  // submitted, the parent component will know to navigate to the correct page
+  const [url, setUrl] = useState("");
+
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     if (token && url) {
-      // Needs refactoring!
-      // Not yet getting token back from FETCH
-      scrapeRecipe(url, token) 
-      setToken(window.localStorage.getItem("token"))
-      navigate('/recipes')
+      navigate("/recipes");
     } else if (token) {
-      navigate('/recipes')
-    } 
-    else {
-      navigate('/login')
+      navigate("/recipes");
+    } else {
+      navigate("/login");
     }
-  }
+  };
 
-  return(
+  return (
     <div className="home items-center">
       {/* Delete the placeholder logo when we have a logo */}
-      <div className="border-2 rounded w-40 h-40">placeholder logo</div> 
+      <div className="border-2 rounded w-40 h-40">placeholder logo</div>
       <h1 className="text-5xl font-bold py-5">RecipEasy</h1>
-      <p className="py-5"> A place to store all your favourite recipes, from ones you find online to creating your own.</p>
-      <RecipeScraper />
-      </div>
+      <p className="py-5">
+        {" "}
+        A place to store all your favourite recipes, from ones you find online
+        to creating your own.
+      </p>
+      <RecipeScraper
+        token={token}
+        url={url}
+        setUrl={setUrl}
+        handleUrlChange={handleUrlChange}
+        handleSubmit={handleSubmit}
+      />
+    </div>
   );
 };
 
--- frontend/tests/pages/homePage.test.jsx
+++ frontend/tests/pages/homePage.test.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter} from "react-router-dom";
 import { userEvent } from "@testing-library/user-event";
 import { HomePage } from "../../src/pages/Home/HomePage";
 import { expect } from "vitest";
-import * as recipeScraper from "../../src/services/recipeScraper";
+// import * as recipeScraper from "../../src/services/recipeScraper";
+import * as scrapeRecipe from '../../src/services/recipe'
 import { vi } from 'vitest'
 
 //Mock useNavigate to test useNavigate logic in isolation
@@ -42,7 +43,7 @@ describe("Home Page: When a user", () => {
       </BrowserRouter>
     );
     const heading = screen.getByRole("heading");
-    const searchbar = screen.getByPlaceholderText("Paste your URL here");
+    const searchbar = screen.getByPlaceholderText("Enter your recipe URL");
     const generateRecipeBtn = screen.getByText("Generate Recipe");
     const enterMaunallyBtn = screen.getByText("Enter Manually");
     const paragraph = screen.getByText("A place to store all your favourite recipes, from ones you find online to creating your own.")
@@ -61,7 +62,7 @@ describe("Home Page: When a user", () => {
       </BrowserRouter>
     );
     const user = userEvent.setup()
-    const searchbar = screen.getByPlaceholderText("Paste your URL here");
+    const searchbar = screen.getByPlaceholderText("Enter your recipe URL");
     await user.type(searchbar, "Hello, world!");
     expect(searchbar.value).toBe("Hello, world!");
   })
@@ -69,7 +70,7 @@ describe("Home Page: When a user", () => {
   test("When a user generates a recipe, the page scrape function is called", async () => {
     // When button is clicked, our mocked function is called
     // Assert the function was called
-    const scrapeRecipeSpy = vi.spyOn(recipeScraper, 'scrapeRecipe')
+    const scrapeRecipeSpy = vi.spyOn(scrapeRecipe, 'scrapeRecipe')
     mockGetItem.mockReturnValueOnce(token)
     render(
       <BrowserRouter>
@@ -78,17 +79,18 @@ describe("Home Page: When a user", () => {
     );
    
     const user = userEvent.setup()
-    const searchbar = screen.getByPlaceholderText("Paste your URL here")
+    const searchbar = screen.getByPlaceholderText("Enter your recipe URL")
     const generateRecipeBtn = screen.getByText("Generate Recipe");
     await user.type(searchbar, url)
     await user.click(generateRecipeBtn);
     expect(scrapeRecipeSpy).toHaveBeenCalledOnce(); 
-    expect(scrapeRecipeSpy).toHaveBeenLastCalledWith(url, token)
+    // expect(scrapeRecipeSpy).toHaveBeenLastCalledWith(url, token)
+    expect(scrapeRecipeSpy).toHaveBeenLastCalledWith(url)
     expect(mockUseNavigate).toHaveBeenCalledWith('/recipes')
   })
   
   test("When a user isn't logged in and clicks on the 'Generate recipe' button, they are prompted to 'Sign Up'", async () => {
-    const scrapeRecipeSpy = vi.spyOn(recipeScraper, 'scrapeRecipe')
+    const scrapeRecipeSpy = vi.spyOn(scrapeRecipe, 'scrapeRecipe')
     render(
       <BrowserRouter>
         <HomePage />
@@ -96,7 +98,7 @@ describe("Home Page: When a user", () => {
     );
    
     const user = userEvent.setup()
-    const searchbar = screen.getByPlaceholderText("Paste your URL here")
+    const searchbar = screen.getByPlaceholderText("Enter your recipe URL")
     const generateRecipeBtn = screen.getByText("Generate Recipe");
     await user.type(searchbar, url)
     await user.click(generateRecipeBtn);
