7a56a2e9b02f30b29aa6d34356c31024a8df2d09
Reece Owen <reece_owen@hotmail.co.uk>
2024-02-28 16:46:39 +0000

    initial start on styling the login page

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -30,23 +30,23 @@ export const LoginPage = () => {
 
   return (
     <>
-      <h2>Login</h2>
+      <h2 className="text-5xl font-bold py-5">Login</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
-        <input
+        <input className="border-2 rounded w-full"
           id="email"
           type="text"
           value={email}
           onChange={handleEmailChange}
         />
         <label htmlFor="password">Password:</label>
-        <input
+        <input className="border-2 rounded w-full"
           id="password"
           type="password"
           value={password}
           onChange={handlePasswordChange}
         />
-        <input role="submit-button" id="submit" type="submit" value="Submit" />
+        <input role="submit-button" id="submit" type="submit" value="Submit" className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900" />
       </form>
     </>
   );
