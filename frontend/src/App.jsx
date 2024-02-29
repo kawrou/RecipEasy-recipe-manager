57113f6977a5fb2afedc72d06b699d3f387b1c31
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-02-29 11:06:22 +0000

    Fixed from merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import HomePage  from "./pages/Home/HomePage";
+import HomePage from "./pages/Home/HomePage";
 import { LoginPage } from "./pages/Login/LoginPage";
 import { SignupPage } from "./pages/Signup/SignupPage";
+import { RecipePage } from "./pages/RecipePage/RecipePage";
 import Navbar from "./components/Navbar";
 
 // docs: https://reactrouter.com/en/main/start/overview
