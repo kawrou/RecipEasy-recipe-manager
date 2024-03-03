7f9e92b9c5a0a986c834273e0236c24231d44170
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-03-03 20:46:53 +0000

    Ingredients edit mode working now

--- frontend/src/components/RecipePage/RecipeFields/IngredientList.jsx
+++ frontend/src/components/RecipePage/RecipeFields/IngredientList.jsx
@@ -29,41 +29,48 @@ export const IngredientList = ({ recipeIngredients, setRecipeIngredients, editMo
       <div className="flex flex-col">
         <div className="flex flex-col divide-y-2">
           {recipeIngredients.map((ingredient, index) => (
-            <div key={index} className="relative py-4">
-              <input
-                className="resize-none overflow-hidden placeholder:text-wrap w-full p-2.5 focus:outline-none text-md text-gray-900 bg-gray-50 rounded-xl border border-gray-300"
-                value={ingredient}
-                onChange={(e) => {
-                  const updatedIngredients = [...recipeIngredients];
-                  updatedIngredients[index] = e.target.value;
-                  setRecipeIngredients(updatedIngredients);
-                }}
-                placeholder="Enter your ingredient..."
-              />
-              <button
-                className="absolute right-3 top-1/2 transform -translate-y-1/2 px-2 py-1 rounded text-white"
-                onClick={() => handleRemoveIngredient(index)}
-              >
-                <FaTimes style={{ color: "gray" }} />{" "}
-                {/* Change color to gray */}
-              </button>
-            </div>
+            editMode ? (
+              <div key={index} className="relative py-4">
+                <input
+                  className="resize-none overflow-hidden placeholder:text-wrap w-full p-2.5 focus:outline-none text-md text-gray-900 bg-gray-50 rounded-xl border border-gray-300"
+                  value={ingredient}
+                  onChange={(e) => {
+                    const updatedIngredients = [...recipeIngredients];
+                    updatedIngredients[index] = e.target.value;
+                    setRecipeIngredients(updatedIngredients);
+                  }}
+                  placeholder="Enter your ingredient..."
+                />
+                <button
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 px-2 py-1 rounded text-white"
+                  onClick={() => handleRemoveIngredient(index)}
+                >
+                  <FaTimes style={{ color: "gray" }} />{" "}
+                  {/* Change color to gray */}
+                </button>
+              </div>
+            ) : (
+              <div key={index} className="text-left text-md py-2.5">{ingredient}</div>
+            )
           ))}
         </div>
 
         {/* Input for new ingredient */}
-        <div className="flex justify-center items-center">
-          <div className="w-1/2 border border-gray-200" />{" "}
-          {/* Horizontal divider */}
-          <button
-            className="px-2 py-1 rounded text-white"
-            onClick={handleAddIngredient}
-          >
-            <FaPlus style={{ color: "gray" }} /> {/* Change color to gray */}
-          </button>
-          <div className="w-1/2 border border-gray-200" />{" "}
-          {/* Horizontal divider */}
-        </div>
+        {editMode && (
+          <div className="flex justify-center items-center">
+            <div className="w-1/2 border border-gray-200" />{" "}
+            {/* Horizontal divider */}
+            <button
+              className="px-2 py-1 rounded text-white"
+              onClick={handleAddIngredient}
+            >
+              <FaPlus style={{ color: "gray" }} /> {/* Change color to gray */}
+            </button>
+            <div className="w-1/2 border border-gray-200" />{" "}
+            {/* Horizontal divider */}
+          </div>
+        )}
+
       </div>
     </div>
   );
