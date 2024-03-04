12158ba891501acd83b921b15638476f7091f840
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-03-04 22:28:55 +0000

    getRecipeById test driven and implemented

--- api/controllers/recipes.js
+++ api/controllers/recipes.js
@@ -4,89 +4,92 @@ const puppeteer = require("puppeteer");
 
 const { generateToken } = require("../lib/token");
 
-const Recipe = require('../models/recipe')
-const User = require('../models/user')
+const Recipe = require("../models/recipe");
+const User = require("../models/user");
 
 const fetchRecipeData = async (req, res) => {
-    const url = req.query.url;
-    console.log("Fetching URL:", url);
+  const url = req.query.url;
+  console.log("Fetching URL:", url);
 
-    try {
-      const response = await axios.get(url);
-      const html = response.data;
-      console.log(response)
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    console.log(response);
 
-      const $ = cheerio.load(html);
+    const $ = cheerio.load(html);
 
-      const jsonLD = $('script[type="application/ld+json"]');
-      let recipeData;
+    const jsonLD = $('script[type="application/ld+json"]');
+    let recipeData;
 
-      jsonLD.each((index, element) => {
-        const scriptContent = $(element).html();
-        try {
-          const jsonData = JSON.parse(scriptContent);
+    jsonLD.each((index, element) => {
+      const scriptContent = $(element).html();
+      try {
+        const jsonData = JSON.parse(scriptContent);
 
-          if (jsonData["@graph"]) {
-            const recipeObjects = jsonData["@graph"].filter((obj) => obj["@type"] === "Recipe");
-            recipeData = recipeObjects;
-          } else if (jsonData["@type"] === "Recipe") {
-            recipeData = jsonData;
-          }
-        } catch (error) {
-          console.error("Error parsing JSON:", error);
+        if (jsonData["@graph"]) {
+          const recipeObjects = jsonData["@graph"].filter(
+            (obj) => obj["@type"] === "Recipe"
+          );
+          recipeData = recipeObjects;
+        } else if (jsonData["@type"] === "Recipe") {
+          recipeData = jsonData;
         }
-      });
-
-      if (!recipeData || recipeData.length === 0) {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        const jsonLdData = await page.$$eval(
-          'script[type="application/ld+json"]',
-          (scripts) => {
-            let jsonData;
-
-            scripts.forEach((script) => {
-              try {
-                const parsedData = JSON.parse(script.textContent);
-
-                if (parsedData["@graph"]) {
-                  const recipeObjects = parsedData["@graph"].filter((obj) => obj["@type"] === "Recipe");
-                  jsonData = recipeObjects;
-                } else if (parsedData["@type"] === "Recipe") {
-                  jsonData = parsedData;
-                }
-              } catch (error) {
-                console.error("Error parsing JSON-LD data:", error);
-              }
-            });
-            return jsonData;
-          }
-        );
-
-        recipeData = jsonLdData;
-
-        await browser.close();
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
       }
+    });
 
-      console.log("Recipe Data:", recipeData);
-      res.status(200).json({ recipe_data: recipeData });
-    } catch (error) {
-      console.log(Object.keys(error))
-      // console.error("Error fetching URL:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+    if (!recipeData || recipeData.length === 0) {
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.goto(url);
+
+      const jsonLdData = await page.$$eval(
+        'script[type="application/ld+json"]',
+        (scripts) => {
+          let jsonData;
+
+          scripts.forEach((script) => {
+            try {
+              const parsedData = JSON.parse(script.textContent);
+
+              if (parsedData["@graph"]) {
+                const recipeObjects = parsedData["@graph"].filter(
+                  (obj) => obj["@type"] === "Recipe"
+                );
+                jsonData = recipeObjects;
+              } else if (parsedData["@type"] === "Recipe") {
+                jsonData = parsedData;
+              }
+            } catch (error) {
+              console.error("Error parsing JSON-LD data:", error);
+            }
+          });
+          return jsonData;
+        }
+      );
+
+      recipeData = jsonLdData;
+
+      await browser.close();
     }
-  };
 
+    console.log("Recipe Data:", recipeData);
+    res.status(200).json({ recipe_data: recipeData });
+  } catch (error) {
+    console.log(Object.keys(error));
+    // console.error("Error fetching URL:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 const create = async (req, res) => {
   // find the user who created the recipe
   const user = await User.findById(req.user_id);
   if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+    return res.status(404).json({ message: "User not found" });
   }
-  console.log(req.body)
+  console.log(req.body);
   // create recipe
   try {
     const newRecipe = new Recipe({
@@ -104,12 +107,16 @@ const create = async (req, res) => {
       dateAdded: req.body.dateAdded,
     });
     await newRecipe.save();
-    console.log("New recipe created:", newRecipe._id.toString())
+    console.log("New recipe created:", newRecipe._id.toString());
     const newToken = generateToken(req.user_id);
-    res.status(201).json({ message: 'Recipe created successfully', recipe: newRecipe, token: newToken });
+    res.status(201).json({
+      message: "Recipe created successfully",
+      recipe: newRecipe,
+      token: newToken,
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -119,10 +126,10 @@ const updateRecipe = async (req, res) => {
     const recipeId = req.params.recipe_id;
     const user = await User.findById(req.user_id);
     if (!user) {
-      console.log("user not found")
-      return res.status(404).json({ message: 'User not found' });
+      console.log("user not found");
+      return res.status(404).json({ message: "User not found" });
     }
-    const recipeUpdateData = { 
+    const recipeUpdateData = {
       name: req.body.name,
       description: req.body.description,
       ownerId: user._id,
@@ -134,7 +141,7 @@ const updateRecipe = async (req, res) => {
       recipeInstructions: req.body.recipeInstructions,
       url: req.body.url,
       image: req.body.image,
-      dateAdded: req.body.dateAdded
+      dateAdded: req.body.dateAdded,
     };
 
     const updatedRecipe = await Recipe.findOneAndUpdate(
@@ -144,21 +151,35 @@ const updateRecipe = async (req, res) => {
     );
 
     if (!updatedRecipe) {
-      return res.status(404).json({ message: 'Recipe not found' });
+      return res.status(404).json({ message: "Recipe not found" });
     }
     const newToken = generateToken(req.user_id);
-    res.status(200).json({ message: 'Recipe updated successfully', updatedRecipe, token: newToken });
+    res.status(200).json({
+      message: "Recipe updated successfully",
+      updatedRecipe,
+      token: newToken,
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
+const getRecipeById = async (req, res) => {
+  console.log("recipe id ----------------------", req.params.recipe_id);
+  const recipeId = req.params.recipe_id;
+  const recipeData = await Recipe.findById(recipeId);
+  const newToken = generateToken(req.user_id);
+  res
+    .status(200)
+    .json({ recipeData: recipeData, user_id: req.user_id, token: newToken });
+};
 
 const RecipesController = {
   fetchRecipeData: fetchRecipeData,
   create: create,
-  updateRecipe: updateRecipe
-}
+  updateRecipe: updateRecipe,
+  getRecipeById: getRecipeById,
+};
 
 module.exports = RecipesController;
--- api/tests/controllers/recipes.test.js
+++ api/tests/controllers/recipes.test.js
@@ -1,24 +1,52 @@
+const request = require("supertest");
+const JWT = require("jsonwebtoken");
+
 const axios = require("axios");
 const puppeteer = require("puppeteer");
 
-const  RecipesController = require("../../controllers/recipes");
+const RecipesController = require("../../controllers/recipes");
+const Recipe = require("../../models/recipe");
+const User = require("../../models/user");
+
+const app = require("../../app");
 
 jest.mock("axios");
 jest.mock("puppeteer");
 
+require("../mongodb_helper");
+
+const secret = process.env.JWT_SECRET;
+
+const createToken = (userId) => {
+  return JWT.sign(
+    {
+      user_id: userId,
+      // Backdate this token of 5 minutes
+      iat: Math.floor(Date.now() / 1000) - 5 * 60,
+      // Set the JWT token to expire in 10 minutes
+      exp: Math.floor(Date.now() / 1000) + 10 * 60,
+    },
+    secret
+  );
+};
+
+let token;
+
 describe("RecipesController", () => {
-    it("should fetch recipe data successfully", async () => {
+  it("should fetch recipe data successfully", async () => {
     const mockAxiosResponse = {
-    data: "<html><body>Mock HTML content</body></html>",
+      data: "<html><body>Mock HTML content</body></html>",
     };
 
     axios.get.mockResolvedValueOnce(mockAxiosResponse);
     puppeteer.launch.mockResolvedValueOnce({
-    newPage: jest.fn().mockResolvedValueOnce({
+      newPage: jest.fn().mockResolvedValueOnce({
         goto: jest.fn().mockResolvedValueOnce(),
-        $$eval: jest.fn().mockImplementation((selector, callback) => callback([])),
-    }),
-        close: jest.fn().mockResolvedValueOnce(),
+        $$eval: jest
+          .fn()
+          .mockImplementation((selector, callback) => callback([])),
+      }),
+      close: jest.fn().mockResolvedValueOnce(),
     });
 
     const req = { query: { url: "https://example.com" } };
@@ -29,9 +57,9 @@ describe("RecipesController", () => {
     expect(axios.get).toHaveBeenCalledWith("https://example.com");
     // console.log(res.status)
     expect(res.status).toHaveBeenCalledWith(200);
-});
+  });
 
-    it("should handle network failure", async () => {
+  it("should handle network failure", async () => {
     axios.get.mockRejectedValueOnce(new Error("Network error"));
 
     const req = { query: { url: "https://example.com" } };
@@ -40,6 +68,140 @@ describe("RecipesController", () => {
     await RecipesController.fetchRecipeData(req, res);
 
     expect(axios.get).toHaveBeenCalledWith("https://example.com");
-    expect(res.status).toHaveBeenCalledWith(500); 
+    expect(res.status).toHaveBeenCalledWith(500);
+  });
+});
+
+describe("Tests for route /recipes/:recipe_id", () => {
+  const dateAdded = new Date("2024-03-04");
+  const user = new User({
+    username: "pops123",
+    email: "poppy@email.com",
+    password: "Password1!",
+  });
+  const recipe = new Recipe({
+    name: "Easy blueberry muffins",
+    description:
+      "Follow this easy blueberry muffins recipe for a fun weekend bake. You're just a few simple steps away from bite-sized blueberry treats",
+    ownerId: "65e1ed2534ba17d5e7b48f68",
+    tags: ["Baking", "Blueberry"],
+    favouritedByOwner: false,
+    totalTime: 40,
+    recipeYield: 4,
+    recipeIngredient: [
+      "100g unsalted butter softened, plus 1 tbsp, melted, for greasing",
+      "140g golden caster sugar",
+      "2 large eggs",
+      "140g natural yogurt",
+      "1 tsp vanilla extract",
+      "2 tbsp milk",
+      "250g plain flour",
+      "2 tsp baking powder",
+      "1 tsp bicarbonate of soda",
+      "125g pack blueberries (or use frozen)",
+    ],
+    recipeInstructions: [
+      "Heat oven to 200C/180C fan/gas 6 and line a 12-hole muffin tin with paper cases. Beat the butter and caster sugar together until pale and fluffy. Add the eggs and beat in for 1 min, then mix in the yogurt, vanilla extract and milk. Combine the flour, baking powder and bicarb in a bowl with ¼ tsp fine salt, then tip this into the wet ingredients and stir in. Finally, fold in the blueberries and divide the mixture between the muffin cases.",
+      "Bake for 5 mins, then reduce oven to 180C/160C fan/gas 4 and bake for 15-18 mins more until risen and golden, and a cocktail stick inserted into the centre comes out clean.",
+      "Cool in the tin for 10 mins, then carefully lift out onto a wire rack to finish cooling. Will keep for 3-4 days in an airtight container – after a day or two, pop them in the microwave for 10-15 secs on high to freshen up.",
+    ],
+    url: "https://www.bbcgoodfood.com/recipes/blueberry-muffin",
+    image:
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/blueberry-muffin-ee95fd4.jpg?resize=768,574",
+    dateAdded: dateAdded,
+  });
+
+  beforeAll(async () => {
+    await user.save();
+    await recipe.save();
+    token = createToken(user.id);
+    console.log("token ----------------------", token);
+  });
+
+  afterAll(async () => {
+    await User.deleteMany({});
+    await Recipe.deleteMany({});
+  });
+
+  describe("GET, when token is present", () => {
+    test("the response code is 200", async () => {
+      const response = await request(app)
+        .get(`/recipes/${recipe._id}`)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.status).toEqual(200);
     });
+
+    test("returns recipe data for that id", async () => {
+      const response = await request(app)
+        .get(`/recipes/${recipe._id}`)
+        .set("Authorization", `Bearer ${token}`);
+
+      const recipeData = response.body.recipeData;
+
+      expect(recipeData.name).toEqual("Easy blueberry muffins");
+      expect(recipeData.description).toEqual(
+        "Follow this easy blueberry muffins recipe for a fun weekend bake. You're just a few simple steps away from bite-sized blueberry treats"
+      );
+      expect(recipeData.ownerId).toEqual("65e1ed2534ba17d5e7b48f68");
+      expect(recipeData.tags).toEqual(["Baking", "Blueberry"]);
+      expect(recipeData.favouritedByOwner).toEqual(false);
+      expect(recipeData.totalTime).toEqual("40");
+      expect(recipeData.recipeYield).toEqual(4);
+      expect(recipeData.recipeIngredient).toEqual([
+        "100g unsalted butter softened, plus 1 tbsp, melted, for greasing",
+        "140g golden caster sugar",
+        "2 large eggs",
+        "140g natural yogurt",
+        "1 tsp vanilla extract",
+        "2 tbsp milk",
+        "250g plain flour",
+        "2 tsp baking powder",
+        "1 tsp bicarbonate of soda",
+        "125g pack blueberries (or use frozen)",
+      ]);
+      expect(recipeData.recipeInstructions).toEqual([
+        "Heat oven to 200C/180C fan/gas 6 and line a 12-hole muffin tin with paper cases. Beat the butter and caster sugar together until pale and fluffy. Add the eggs and beat in for 1 min, then mix in the yogurt, vanilla extract and milk. Combine the flour, baking powder and bicarb in a bowl with ¼ tsp fine salt, then tip this into the wet ingredients and stir in. Finally, fold in the blueberries and divide the mixture between the muffin cases.",
+        "Bake for 5 mins, then reduce oven to 180C/160C fan/gas 4 and bake for 15-18 mins more until risen and golden, and a cocktail stick inserted into the centre comes out clean.",
+        "Cool in the tin for 10 mins, then carefully lift out onto a wire rack to finish cooling. Will keep for 3-4 days in an airtight container – after a day or two, pop them in the microwave for 10-15 secs on high to freshen up.",
+      ]);
+      expect(recipeData.url).toEqual(
+        "https://www.bbcgoodfood.com/recipes/blueberry-muffin"
+      );
+      expect(recipeData.image).toEqual(
+        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/blueberry-muffin-ee95fd4.jpg?resize=768,574"
+      );
+      expect(recipeData.dateAdded).toEqual("2024-03-04T00:00:00.000Z");
+    });
+
+    test("returns a new token", async () => {
+      const response = await request(app)
+        .get(`/recipes/${recipe._id}`)
+        .set("Authorization", `Bearer ${token}`);
+
+      const newToken = response.body.token;
+      const newTokenDecoded = JWT.decode(newToken, process.env.JWT_SECRET);
+      const oldTokenDecoded = JWT.decode(token, process.env.JWT_SECRET);
+
+      // iat stands for issued at
+      expect(newTokenDecoded.iat > oldTokenDecoded.iat).toEqual(true);
+    });
+  });
+
+  describe("GET, when token is missing", () => {
+    test("the response code is 401", async () => {
+      const response = await request(app).get(`/recipes/${recipe._id}`);
+      expect(response.status).toEqual(401);
+    });
+
+    test("returns no recipe data", async () => {
+      const response = await request(app).get(`/recipes/${recipe._id}`);
+      expect(response.body.recipeData).toEqual(undefined);
+    });
+
+    test("does not return a new token", async () => {
+      const response = await request(app).get(`/recipes/${recipe._id}`);
+      expect(response.body.token).toEqual(undefined);
+    });
+  });
 });
