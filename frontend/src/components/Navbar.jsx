2bf547fd1c19f53cfb5801f270aa26f70f4db7bf
Gabriela <gabrielaehrenbrink@gmail.com>
2024-03-04 12:16:48 +0000

    test for logout button added, to be updated once recipe collection page is done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
+
+
 const Navbar = ({ isLoggedIn, onLogout }) => {
   const [isOpen, setOpen] = useState(false);
 
