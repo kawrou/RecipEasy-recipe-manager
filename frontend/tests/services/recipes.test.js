bc053ff2eccc5f7f0022aeda730ec5af424da089
Alan Gardiner <121672581+kawrou@users.noreply.github.com>
2024-03-05 20:22:18 +0000

    Fixed recipes.test for services/recipes

--- frontend/tests/services/recipes.test.js
+++ frontend/tests/services/recipes.test.js
@@ -1,44 +1,58 @@
 import createFetchMock from "vitest-fetch-mock";
-import { scrapeRecipe } from '../../src/services/recipe';
+import * as recipeService from "../../src/services/recipes";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 createFetchMock(vi).enableMocks();
 
 describe("recipe service", () => {
-    describe("Scrape recipe GET request", () => {
-        test("successful request returns data", async () => {
-            const mockUrl = `${BACKEND_URL}/recipes/scrape-recipe`;
+  describe("Scrape recipe GET request", () => {
+    //Test that when you send a request with a url and a token, you get back a new token and some data
+    test("successful request returns data", async () => {
+      const mockUrl = `${BACKEND_URL}/recipes/scrape-recipe`;
+      const mockToken = "test token";
 
+      fetch.mockResponseOnce(
+        JSON.stringify({ recipeData: [], token: "newToken" }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
 
-            fetch.mockResponseOnce(JSON.stringify({ recipeData: [], token: "newToken" }), {
-                status: 200,
-                headers: { 'Content-Type': 'application/json' },
-            });
+      const result = await recipeService.scrapeRecipe(mockUrl, mockToken);
 
+      expect(result.recipeData).toEqual([]);
+      expect(result.token).toEqual("newToken");
 
-            const result = await scrapeRecipe(mockUrl);
+      const fetchArguments = fetch.mock.lastCall;
+      const url = fetchArguments[0];
+      const options = fetchArguments[1];
 
-
-            expect(result.recipeData).toEqual([]);
-            expect(result.token).toEqual("newToken");
-
-
-            const fetchArguments = fetch.mock.lastCall;
-            const url = fetchArguments[0];
-            const options = fetchArguments[1];
-
-            expect(url).toEqual(`${BACKEND_URL}/recipes/scrape-recipe?url=${encodeURIComponent(mockUrl)}`);
-        });
-
-        test("failed request throws an error", async () => {
-            const mockUrl = `${BACKEND_URL}/recipes/scrape-recipe`;
-
-            fetch.mockResponseOnce(JSON.stringify({ error: "Failed to fetch recipe" }), {
-                status: 500, 
-                headers: { 'Content-Type': 'application/json' },
-            });
-            await expect(scrapeRecipe(mockUrl)).rejects.toThrow("Unable to make GET request for fetch recipe");
-        });
+      expect(url).toEqual(
+        `${BACKEND_URL}/recipes/scrape-recipe?url=${encodeURIComponent(
+          mockUrl
+        )}`
+      );
+      expect(options).toEqual({
+        method: "GET",
+        headers: { Authorization: "Bearer test token" },
+      });
     });
+
+    test("failed request throws an error", async () => {
+      const mockUrl = `${BACKEND_URL}/recipes/scrape-recipe`;
+
+      fetch.mockResponseOnce(
+        JSON.stringify({ error: "Failed to fetch recipe" }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      await expect(recipeService.scrapeRecipe(mockUrl)).rejects.toThrow(
+        "Unable to make GET request for fetch recipe"
+      );
+    });
+  });
 });
