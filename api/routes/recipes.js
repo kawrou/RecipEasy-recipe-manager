bececdf8c7c727633e2056b96b3ba5e37628461f
Alan Gardiner <121672581+kawrou@users.noreply.github.com>
2024-03-06 11:28:21 +0000

    Deleted: getRecipes - deprecated

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -7,7 +7,7 @@ const RecipesController = require("../controllers/recipes.js")
 // const getRecipesController = require("../controllers/getRecipes")
 
 const router = express.Router();
-const tokenChecker = require("../middleware/tokenChecker");
+// const tokenChecker = require("../middleware/tokenChecker");
 
 router.get("/scrape-recipe", RecipesController.fetchRecipeData);
 router.get("/:recipe_id", RecipesController.getRecipeById);
@@ -16,6 +16,4 @@ router.post("/", RecipesController.create);
 // patch request most suitable - https://www.geeksforgeeks.org/what-is-the-difference-between-put-post-and-patch-in-restful-api/
 router.patch("/:recipe_id", RecipesController.updateRecipe);
 router.get('/myrecipes/:user_id', RecipesController.getAllRecipesByUserId); 
-// Can delete once all tests resolve. 
-// router.get('/:user_id', getRecipesController.getAllRecipesByUserId); 
 module.exports = router;
