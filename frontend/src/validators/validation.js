3ac00aeffbdc3c7d5dbb25e344005551c345d226
kawrou <121672581+kawrou@users.noreply.github.com>
2024-03-26 01:23:26 +0000

    Test past for simple login form validation

--- frontend/src/validators/validation.js
+++ frontend/src/validators/validation.js
@@ -1,6 +1,11 @@
+export const ieEmpty = (value) => {
+    return !value.trim(); 
+}
+
 export const validateLoginForm = (email, password) => {
 
     const validationMsg = {};
+    
     if (!email.trim()) {
       validationMsg.email =
         "Email address field was empty. Please enter an email address";
@@ -10,13 +15,11 @@ export const validateLoginForm = (email, password) => {
       validationMsg.password =
         "Password field was empty. Please enter your password";
     }
-
-    setValidation(validationMsg);
     
-    if (Object.keys(validationMsg).length > 0){
-      return
-    }
+    // if (Object.keys(validationMsg).length > 0){
+    //   return
+    // }
 
 
-    return
+    return validationMsg
 }
--- frontend/tests/validators/validation.test.js
+++ frontend/tests/validators/validation.test.js
@@ -1,13 +1,18 @@
+import { expect } from "vitest";
 import { validateLoginForm } from "../../src/validators/validation";
 
 
 describe("Login form validation", () => {
-    test("IF email string is empty, return a validation object with appropriate validation message", () => {
+    test("If email string is empty, return a validation object with appropriate validation message", () => {
+        const result = validateLoginForm("", "testPassword")
 
+        expect(result).toEqual({email: "Email address field was empty. Please enter an email address"})
     })
 
     test("If password string is empty, return a validation object with appropriate validation message", () => {
-        
+        const result = validateLoginForm("test@test.com", "")
+
+        expect(result).toEqual({password: "Password field was empty. Please enter your password"})
     })
 })
 //If email string is empty, return a validation object containing the string "Email address field was empty. Please enter an email address"
