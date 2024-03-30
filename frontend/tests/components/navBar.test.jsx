57736580eae107c6096fb6a924ae7addac34b011
kawrou <121672581+kawrou@users.noreply.github.com>
2024-03-30 02:09:45 +0000

    Finished testing NavBar navigation when on MyRecipesPage

--- frontend/tests/components/navBar.test.jsx
+++ frontend/tests/components/navBar.test.jsx
@@ -1,5 +1,5 @@
-import { cleanup, render, screen, waitFor } from "@testing-library/react";
-import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import { describe, test, vi } from "vitest";
 import Navbar from "../../src/components/Navbar";
@@ -11,13 +11,16 @@ import { MyRecipesPage } from "../../src/pages/MyRecipes/MyRecipesPage";
 
 const user = userEvent.setup();
 
-// const initialRoutingState = {
-//   initialEntries: ["/"], // Set initial URL to '/'
-// };
+vi.mock("../../src/hooks/useFetchRecipe", () => {
+  const useFetchRecipesMock = vi.fn().mockReturnValue({
+    recipes: [],
+    loading: true,
+    error: null,
+    fetchRecipes: vi.fn(),
+  });
+  return { useFetchRecipes: useFetchRecipesMock };
+});
 
-const onLogoutMock = vi.fn();
-
-const onLoginMock = vi.fn();
 const setTokenMock = vi.fn();
 
 describe("Navbar", () => {
@@ -35,38 +38,6 @@ describe("Navbar", () => {
       );
     });
 
-    // beforeEach(() => {
-    //   render(
-    //     <BrowserRouter
-    //       isLoggedIn={false}
-    //       onLogout={onLogoutMock}
-    //       initialEntries={["/"]}
-    //     >
-    //       <Navbar />
-    //     </BrowserRouter>
-    //   );
-    // });
-    // afterEach(() => {
-    //   cleanup();
-    // });
-
-    // test.skip("Navbar is visible with Home, Login, Signup buttons that redirect to their routes", async () => {
-    //   // render(
-    //   //   <BrowserRouter>
-    //   //     <Navbar />
-    //   //   </BrowserRouter>
-    //   // );
-
-    //   const loginLink = screen.getByText("Log In");
-    //   expect(loginLink.getAttribute("href")).toEqual("/login");
-
-    //   const signupLink = screen.getByText("Sign Up");
-    //   expect(signupLink.getAttribute("href")).toEqual("/signup");
-
-    //   const homeLink = screen.getByText("Home");
-    //   expect(homeLink.getAttribute("href")).toEqual("/");
-    // });
-
     test("'logo' navigates to home page", async () => {
       const logoEl = screen.getByAltText("Recipeasy Homepage Logo Link");
       await user.click(logoEl);
@@ -81,8 +52,6 @@ describe("Navbar", () => {
     test("'Log In' button navigates to login page", async () => {
       const logInBtnEl = screen.getByRole("link", { name: "Log in" });
       await user.click(logInBtnEl);
-
-      // expect(window.location.pathname).toBe("/login");
 
       const headingEl = screen.getByRole("heading", {
         name: "Log in to your account",
@@ -122,56 +91,116 @@ describe("Navbar", () => {
     test("'My Recipes' button navigates to My Recipes page", async () => {
       const myRecipesBtn = screen.getByRole("link", { name: "My Recipes" });
       await user.click(myRecipesBtn);
-			screen.debug();
-			const h2El = screen.getByRole("heading", {level: 2})
-			expect(h2El).toBeVisible();
-    });
-    test(
-      "'Log Out' button is visible and navigates to Home Page",
-      async () => {
-        const logOutBtnEl = screen.getByRole("link", { name: "Log Out" });
-        await user.click(logOutBtnEl);
 
-        const headingEl = screen.getByRole("heading", { name: "Recipeasy" });
-        expect(headingEl).toBeVisible();
-      }
-    );
+      const h2El = screen.getByRole("heading", { level: 2 });
+      expect(h2El).toBeVisible();
+    });
+    test("'Log Out' button is visible and navigates to Home Page", async () => {
+      const logOutBtnEl = screen.getByRole("link", { name: "Log Out" });
+      await user.click(logOutBtnEl);
+
+      const headingEl = screen.getByRole("heading", { name: "Recipeasy" });
+      expect(headingEl).toBeVisible();
+    });
     test("'Sign Up' isn't rendered", () => {
-			const signUpBtnEl = screen.queryByRole("link", {name: "Sign Up"}); 
-			expect(signUpBtnEl).not.toBeInTheDocument(); 
-		});
+      const signUpBtnEl = screen.queryByRole("link", { name: "Sign Up" });
+      expect(signUpBtnEl).not.toBeInTheDocument();
+    });
   });
 
   describe("When a user is on the Sign Up page:", () => {
-		beforeEach(() => {
+    beforeEach(() => {
       render(
         <MemoryRouter initialEntries={["/signup"]}>
-          <Navbar isLoggedIn={true} />
+          <Navbar />
           <Routes>
-            {/* <Route path="/" element={<HomePage />} /> */}
             <Route path="/signup" element={<SignupPage />} />
           </Routes>
         </MemoryRouter>
       );
     });
     test("Navbar elements aren't rendered", () => {
-			screen.debug();
-			const logoEl = screen.queryByAltText("Recipeasy Homepage Logo Link")
-			const logInLinkEl = screen.queryByRole("link", {name: "Log in"})
-			const signUpLinkEl = screen.queryByRole("link", {name: "Sign Up"})
+      const logoEl = screen.queryByAltText("Recipeasy Homepage Logo Link");
+      const logInLinkEl = screen.queryByRole("link", { name: "Log in" });
+      const signUpLinkEl = screen.queryByRole("link", { name: "Sign Up" });
 
-			expect(logoEl).not.toBeInTheDocument();
-			expect(logInLinkEl).not.toBeInTheDocument();
-			expect(signUpLinkEl).not.toBeInTheDocument();
-		});
+      expect(logoEl).not.toBeInTheDocument();
+      expect(logInLinkEl).not.toBeInTheDocument();
+      expect(signUpLinkEl).not.toBeInTheDocument();
+    });
   });
+
   describe("When a user is on the Log In page:", () => {
-    test.todo("Navbar elements aren't rendered");
+    beforeEach(() => {
+      render(
+        <MemoryRouter initialEntries={["/login"]}>
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </MemoryRouter>
+      );
+    });
+    test("Navbar elements aren't rendered", () => {
+      const logoEl = screen.queryByAltText("Recipeasy Homepage Logo Link");
+      const logInLinkEl = screen.queryByRole("link", { name: "Log in" });
+      const signUpLinkEl = screen.queryByRole("link", { name: "Sign Up" });
+
+      expect(logoEl).not.toBeInTheDocument();
+      expect(logInLinkEl).not.toBeInTheDocument();
+      expect(signUpLinkEl).not.toBeInTheDocument();
+    });
   });
+
   describe("When a user is on their My Recipes page:", () => {
-    test.todo("'logo' navigates to Home Page");
-    test.todo("'Home' button navigates to Home Page");
-    test.todo("'My Recipes' button navigates to My Recipes page");
-    test.todo("'Log Out' button is visible and navigates to Home Page");
+    beforeEach(() => {
+      render(
+        <MemoryRouter initialEntries={["/myrecipes"]}>
+          <Navbar isLoggedIn={true} />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/myrecipes"
+              element={
+                <MyRecipesPage token={"testToken"} setToken={setTokenMock} />
+              }
+            />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </MemoryRouter>
+      );
+    });
+
+    test("'logo' navigates to Home Page", async () => {
+      const logoEl = screen.getByAltText("Recipeasy Homepage Logo Link");
+      await user.click(logoEl);
+
+      const headingEl = screen.getByRole("heading", { name: "Recipeasy" });
+      expect(headingEl).toBeVisible();
+    });
+
+    test("'Home' button navigates to Home Page", async () => {
+      const homeLinkEl = screen.getByRole("link", { name: "Home" });
+      await user.click(homeLinkEl);
+
+      const headingEl = screen.getByRole("heading", { name: "Recipeasy" });
+      expect(headingEl).toBeVisible();
+    });
+
+    test("'My Recipes' button navigates to My Recipes page", async () => {
+      const myRecipesLinkEl = screen.getByRole("link", { name: "My Recipes" });
+      await user.click(myRecipesLinkEl);
+
+      const headingEl = screen.getByRole("heading", { name: "logo ecipeasy" });
+      expect(headingEl).toBeVisible();
+    });
+
+    test("'Log Out' button is visible and navigates to Home Page", async () => {
+      const logOutLinkEl = screen.getByRole("link", { name: "Log Out" });
+      await user.click(logOutLinkEl);
+
+      const headingEl = screen.getByRole("heading", { name: "Recipeasy" });
+      expect(headingEl).toBeVisible();
+    });
   });
 });
